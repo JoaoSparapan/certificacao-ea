6eabb860bce363f75fcf9a8fb83115b723422421
João Marcelo Sparapan Servilha Oliveira <sparapan15@hotmail.com>
2023-12-19 20:53:30 +0000

    criado dimensão de cartões e dependentes

---
+++ models/staging/sap_adw/stg_sap__cartoes.sql
@@ -0,0 +1,9 @@
+with
+    fonte_cartoes as (
+        select
+            cast(creditcardid as int) as id_cartao
+            , cast(cardtype as string) as tipo_cartao
+        from {{ source('sap-adw', 'creditcard') }}
+    )
+
+select * from fonte_cartoes
