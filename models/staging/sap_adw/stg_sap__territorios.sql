1556b06bcb4c561ca74e6e5a4d21e8ad336fb9f7
João Marcelo Sparapan Servilha Oliveira <sparapan15@hotmail.com>
2023-12-21 18:42:40 +0000

    correção stage território

--- models/staging/sap_adw/stg_sap__territorios.sql
+++ models/staging/sap_adw/stg_sap__territorios.sql
@@ -2,7 +2,7 @@ with
     fonte_territorios as (
         select
             cast(territoryid as int) as id_territorio
-            , cast(name as string) as nome_terriotrio
+            , cast(name as string) as nome_territorio
             , cast(countryregioncode as string) as codigo_pais_territorio
             , cast(`group` as string) as grupo_territorio
         from {{ source('sap_adw', 'salesterritory') }}
