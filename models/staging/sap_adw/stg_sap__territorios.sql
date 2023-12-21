1d16ca6c33a3f5ee1d631690f96fe96d74b705e8
João Marcelo Sparapan Servilha Oliveira <sparapan15@hotmail.com>
2023-12-21 18:07:27 +0000

    Criado stage território para compor dimensão endereços

---
+++ models/staging/sap_adw/stg_sap__territorios.sql
@@ -0,0 +1,11 @@
+with
+    fonte_territorios as (
+        select
+            cast(territoryid as int) as id_territorio
+            , cast(name as string) as nome_terriotrio
+            , cast(countryregioncode as string) as codigo_pais_territorio
+            , cast(`group` as string) as grupo_territorio
+        from {{ source('sap_adw', 'salesterritory') }}
+    )
+
+select * from fonte_territorios
