67314e5f64e6e9691ef4e081b87044bbb9a150c5
João Marcelo Sparapan Servilha Oliveira <sparapan15@hotmail.com>
2023-12-21 19:01:51 +0000

    dimensão endereços incrementada com territórios

--- models/marts/dim_enderecos.sql
+++ models/marts/dim_enderecos.sql
@@ -17,10 +17,15 @@ with
         from {{ ref('stg_sap__paises') }}
     )
 
+    , stg_territorios as (
+        select * from {{ ref('stg_sap__territorios') }}
+    )
+
     , joined_tabelas as (
         select
             row_number() over (order by cidades.id_cidade) as sk_endereco
             , cidades.id_cidade
+            , territorios.id_territorio
             , cidades.nome_cidade
             , cidades.codigo_postal_cidade
             , cidades.endereco_cidade
@@ -28,10 +33,13 @@ with
             , paises.codigo_pais
             , estados.nome_estado
             , paises.nome_pais
+            , territorios.nome_territorio
+            , territorios.grupo_territorio
 
-        from stg_cidades as cidades
-        left join stg_estados as estados on estados.id_estado = cidades.id_estado
-        left join stg_paises as paises on paises.codigo_pais = estados.codigo_pais
+        from stg_cidades cidades
+        left join stg_estados estados on estados.id_estado = cidades.id_estado
+        left join stg_paises paises on paises.codigo_pais = estados.codigo_pais
+        left join stg_territorios territorios on paises.codigo_pais = territorios.codigo_pais_territorio
     )
 
 select * 
