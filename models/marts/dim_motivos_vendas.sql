179124712292e638e929e176750ff518e64b897f
João Marcelo Sparapan Servilha Oliveira <sparapan15@hotmail.com>
2023-12-27 20:12:02 +0000

    relação motivo e venda refeito

--- models/marts/dim_motivos_vendas.sql
+++ models/marts/dim_motivos_vendas.sql
@@ -13,27 +13,11 @@ with
 
     , joined_tabelas as (
         select
-            {{ dbt_utils.generate_surrogate_key(['pedidos_motivos.id_motivo']) }} as sk_motivo_venda
+            {{ dbt_utils.generate_surrogate_key(['pedidos_motivos.id_motivo','pedidos_motivos.id_pedido','motivos_vendas.nome_motivo']) }} as sk_motivo_venda
             , pedidos_motivos.id_pedido
             , motivos_vendas.nome_motivo
         from stg_pedidos_motivos pedidos_motivos
         left join stg_motivos_vendas motivos_vendas on pedidos_motivos.id_motivo = motivos_vendas.id_motivo
     )
 
-    , transformation as (
-        select
-            id_pedido        
-            , string_agg(nome_motivo, ', ') as nome_motivo_temp
-        from joined_tabelas
-        group by id_pedido
-    )
-    
-    , add_sk as (
-        select
-            {{ dbt_utils.generate_surrogate_key(['transformation.id_pedido']) }} as sk_motivo_venda
-            , transformation.id_pedido
-            , ifnull(transformation.nome_motivo_temp,'Desconhecido') as nome_motivo
-        from transformation
-    )
-
-select * from add_sk
+select * from joined_tabelas
